538899d44b9332ab4c1db3d8b253c243437640d8
Mehrad <mehrad.kavian@gmail.com>
2020-01-02 18:55:24 +0000

    Loops covered, moving to functions

--- src/loops.rs
+++ src/loops.rs
@@ -4,8 +4,39 @@ pub fn run (){
     let mut count = 0;
 
     //infinite loop
-    loop {
-        count+=1;
-        println!("Numer :{}", count);
+    // loop {
+    //     count+=1;
+    //     println!("Numer :{}", count);
+    //     if count == 20 {
+    //         break;
+    //     }
+    // }
+    
+    // While Loop(FizzBuzz)
+    // while count<= 100 {
+    //     if count % 15 == 0 {
+    //         println!("FizzBuzz" );
+    //     }else if count % 3 ==0 {
+    //         println!("Fizz");
+    //     } else if count % 5  ==0 {
+    //         println!("Buzz");
+    //     }else {
+    //         println!("{}",count );
+    //     }
+    //     //Increment
+    //     count+=1;
+    // }
+
+    // For range
+    for x in 0..100 {
+        if x % 15 == 0 {
+            println!("FizzBuzz" );
+        }else if x % 3 ==0 {
+            println!("Fizz");
+        } else if x % 5  ==0 {
+            println!("Buzz");
+        }else {
+            println!("{}",x );
+        }
     }
 }
