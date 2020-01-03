a70d7643112e41c0ade5215d56c171586b6620b2
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-01-03 13:48:30 -0500

    hello world

---
+++ lynda/hello_cargo/target/debug/deps/hello_cargo-4a60fba4cb6bc7df.d
@@ -0,0 +1,5 @@
+/Users/mehradkavian/Dropbox/Rust/rustSandBox/lynda/hello_cargo/target/debug/deps/hello_cargo-4a60fba4cb6bc7df.rmeta: src/main.rs
+
+/Users/mehradkavian/Dropbox/Rust/rustSandBox/lynda/hello_cargo/target/debug/deps/hello_cargo-4a60fba4cb6bc7df.d: src/main.rs
+
+src/main.rs:
---
+++ lynda/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/mehradkavian/Dropbox/Rust/rustSandBox/lynda/hello_cargo/target/debug/hello_cargo: /Users/mehradkavian/Dropbox/Rust/rustSandBox/lynda/hello_cargo/src/main.rs
