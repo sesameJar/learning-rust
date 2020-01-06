1fd1b5f56fde2c6acc8a54bc584c24c160cdcc5f
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-01-06 17:55:48 -0500

    adding comparison functionality

--- lynda/guessing_game/src/main.rs
+++ lynda/guessing_game/src/main.rs
@@ -1,9 +1,11 @@
 extern crate rand;
 
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    println!("Wlecome to the guessing game");
     let secret = rand::thread_rng().gen_range(1, 101);
 
     println!("Please input your guess :");
@@ -15,5 +17,10 @@ fn main() {
         .expect("failed to read line!");
 
     println!("You guessed {}", guess);
-    
+    let guess: u32 = guess.trim().parse().expect("Please Type a number.");
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("Awesome!!!!!!"),
+    };
 }
