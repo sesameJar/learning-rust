38fd7938f92cf1c464cf01197579a53d25c243b0
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-01-06 17:44:28 -0500

    getting user input or user's guess

---
+++ lynda/guessing_game/target/debug/deps/guessing_game-7bd62f9f2e5d4b25.d
@@ -0,0 +1,5 @@
+/Users/mehradkavian/Dropbox/Rust/rustSandBox/lynda/guessing_game/target/debug/deps/guessing_game-7bd62f9f2e5d4b25: src/main.rs
+
+/Users/mehradkavian/Dropbox/Rust/rustSandBox/lynda/guessing_game/target/debug/deps/guessing_game-7bd62f9f2e5d4b25.d: src/main.rs
+
+src/main.rs:
---
+++ lynda/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/mehradkavian/Dropbox/Rust/rustSandBox/lynda/guessing_game/target/debug/guessing_game: /Users/mehradkavian/Dropbox/Rust/rustSandBox/lynda/guessing_game/src/main.rs
